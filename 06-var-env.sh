1eb6d1da467d5c2056904f5ac3fc69f01d1f305a
Anjana <anjanarajasekhar06963@gmail.com>
2026-08-02 22:50:34 +0530

    shellscript

--- 06-var-env.sh
+++ 06-var-env.sh
@@ -1,4 +1,4 @@
 #/bin/bash
-export COURSE="DevOPs" #export is environmental variables and it valid till the session
+export COURSE="DevOps" #export is environmental variables and it valid till the session
 
 echo $COURSE

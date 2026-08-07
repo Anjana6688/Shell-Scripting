9cad7d3b8709fe19bcb1706e6cb7ec474b0a8634
Anjana <anjanarajasekhar06963@gmail.com>
2026-08-07 22:47:26 +0530

    shellscript

--- 16-logs.sh
+++ 16-logs.sh
@@ -23,10 +23,10 @@ fi
 
 VALIDATE(){ # functions receive inputs through args just like shell script args
     if [ $1 -ne 0 ]; then
-        echo -e "Installing $2 ... $R FAILURE $N" 
+        echo -e "Installing $2 ... $R FAILURE $N"  | tee -a $LOG_FILE
         exit 1
     else
-        echo -e "Installing $2 ... $G SUCCESS $N"
+        echo -e "Installing $2 ... $G SUCCESS $N" | tee -a $LOG_FILE
     fi
 }
 
